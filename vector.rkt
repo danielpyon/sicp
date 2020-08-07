e517c76812e22ecf5cfc3a59579aab4e349e584e
Daniel Pyon <daniel@quicksat.com>
2020-08-07 12:33:42 -0700

    scale-list

--- vector.rkt
+++ vector.rkt
@@ -4,3 +4,12 @@
 (define xcor car)
 (define ycor cdr)
 
+(define make-segment cons)
+(define seg-start car)
+(define seg-end cdr)
+
+(define (scale-list s l)
+  (if (null? 1)
+      nil
+      (cons ((* (car l) s))
+            ((scale-list s (cdr l))))))
