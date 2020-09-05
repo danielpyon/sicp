d886092dc985b9b1d4cff65910651885dba43b6d
Daniel Pyon <daniel@quicksat.com>
2020-09-05 13:20:54 -0700

    euler approx

--- 1.37.rkt
+++ 1.37.rkt
@@ -1,12 +1,15 @@
 #lang racket
+(provide cont-frac)
 
-(define (cont-frac-iter n d k c)
+(define (cont-frac-recur n d k c)
 	(if (= k c)
 		(/ (n c) (d c))
-		(/ (n c) (+ (d c) (cont-frac-iter n d k (+ c 1))))))
-(define (cont-frac n d k)
-	(cont-frac-iter n d k 0))
+		(/ (n c) (+ (d c) (cont-frac-recur n d k (+ c 1))))))
 
-(println (cont-frac (lambda (i) 1.0)
-					(lambda (i) 1.0)
-					10))
+(define (cont-frac n d k)
+	(cont-frac-recur n d k 0))
+
+(define phi (cont-frac
+	(lambda (i) 1.0)
+	(lambda (i) 1.0)
+	10))
---
+++ 1.38.rkt
@@ -0,0 +1,14 @@
+#lang racket
+(require "1.37.rkt")
+
+; Euler's e approximation
+(define e
+	(+ 2
+		(cont-frac
+			(lambda (i) 1.0)
+			(lambda (i)
+				(if (= (remainder i 3) 2) i 1.0))
+			1000)))
+
+(println e)
+
