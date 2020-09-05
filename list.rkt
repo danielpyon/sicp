5e8ad52757fc9e41b4ce00b056a6f8d90800ae11
Daniel Pyon <daniel@quicksat.com>
2020-09-04 22:15:09 -0700

    foreach

--- list.rkt
+++ list.rkt
@@ -3,4 +3,9 @@
 (define (map f l)
   (if (empty? l)
       '()
-      (list (f (car l)) (map (cdr l)))))
+      (list (f (car l)) (map f (cdr l)))))
+
+(define (for-each f l)
+  (cond ((empty? l) "done")
+        (else (f (car l))
+              (for-each f (cdr l)))))
