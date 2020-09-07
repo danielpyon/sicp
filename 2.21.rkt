32656e6fc81fc263b9711db3e1ea8bc7508b3f66
Daniel Pyon <daniel@quicksat.com>
2020-09-07 13:33:11 -0700

    2.21.rkt

---
+++ 2.21.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(define (square x) (* x x))
+
+(define (square-list items)
+	(if
+		(null? items)
+			null
+			(cons (square (car items)) (square-list (cdr items)))))
+
+;(define (square-list items)
+	;(map (lambda (x) (square x)) items))
+
+
