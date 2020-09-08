c58169647c431056c749d0522fc4a01ef4176883
Daniel Pyon <daniel@quicksat.com>
2020-09-07 22:36:12 -0700

    fixed tree map

--- 2.31.rkt
+++ 2.31.rkt
@@ -7,4 +7,6 @@
 	(cond
 		((null? tree) null)
 		((not (pair? tree)) (f tree))
-		(else (cons (tree-map (car tree)) (tree-map (cdr tree))))
+		(else (cons (tree-map f (car tree)) (tree-map f (cdr tree))))))
+
+(tree-map (lambda (x) (* x x)) (list 1 (list 2 (list 3 4) 5) (list 6 7)))
