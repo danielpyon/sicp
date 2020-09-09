77e3e6583203e602e2ebfdbed23f45b18a470713
Daniel Pyon <daniel@quicksat.com>
2020-09-08 23:17:53 -0700

    2.37 not done yet

--- 2.37.rkt
+++ 2.37.rkt
@@ -7,12 +7,20 @@
 		initial
 		(op (car sequence) (accumulate op initial (cdr sequence)))))
 
+(define (accumulate-n op init seqs)
+	(if (null? (car seqs))
+		null
+		(cons
+			(accumulate op init (map car seqs))
+			(accumulate-n op init (map cdr seqs)))))
+
 (define (dot-product v w)
 	(accumulate + 0 (map * v w)))
 
 (define (matrix-*-vector m v)
 	(map
 		(lambda (x) (dot-product x v))
-		m))
+	m))
 
+(accumulate-n + 0 '((1 2 3) (4 5 6)))
 (matrix-*-vector '((1 2 3) (4 5 6) (7 8 9)) '(1 2 3))
