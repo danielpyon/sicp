bd1edb2790f55846620de0c26aed5ab63963b284
Daniel Pyon <daniel@quicksat.com>
2020-09-10 17:52:26 -0700

    minor simplification

--- 2.56.rkt
+++ 2.56.rkt
@@ -8,8 +8,22 @@
 		(variable? v1)
 		(variable? v2)
 		(eq? v1 v2)))
-(define (make-sum a1 a2) (list '+ a1 a2))
-(define (make-product m1 m2) (list '* m1 m2))
+(define (=number? expr val)
+	(and (number? expr) (= val expr)))
+(define (make-sum a1 a2)
+	(cond
+		((=number? a1 0) a2)
+		((=number? a2 0) a1)
+		((and (number? a1) (number? a2)) (+ a1 a2))
+		(else (list '+ a1 a2))))
+(define (make-product m1 m2)
+	(cond
+		((=number? m1 0) 0)
+		((=number? m2 0) 0)
+		((=number? m1 1) m2)
+		((=number? m2 1) m1)
+		((and (number? m1) (number? m2)) (* m1 m2))
+		(else (list '* m1 m2))))
 (define (sum? x) (and (pair? x) (eq? '+ (car x))))
 (define (addend s) (cadr s))
 (define (augend s) (caddr s))
@@ -34,6 +48,6 @@
 		(else
 			(error "unknown expression type: DERIV" expr))))
 
-; 3x^3 + 4x^2 + x + 3 => 9x + 8x + 1
+; 3x^3 + 4x^2 + x + 3 => 9x^2 + 8x + 1
 (deriv '(+ (* 3 x x x) (* 4 x x) x 3) 'x)
 (deriv '(* x y) 'x)
