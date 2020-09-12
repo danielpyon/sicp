9cfb4c3391421d344a0870cb9811775c9845b365
Daniel Pyon <daniel@quicksat.com>
2020-09-12 14:02:02 -0700

    adjoin set for ordered list implementation

--- 2.61.rkt
+++ 2.61.rkt
@@ -16,6 +16,18 @@
 		((> (car set1) (car set2)) (intersection-set set1 (cdr set2)))
 		(else (cons (car set1) (intersection-set (cdr set1) (cdr set2))))))
 
+(define (adjoin-set set1 set2)
+	(define (sort-asc l)
+		(sort l (lambda (x y) (< x y))))
+	(cond
+		((null? set1) set2)
+		((null? set2) set1)
+		((not (= (car set1) (car set2)))
+			(sort-asc (cons (car set1) (cons (car set2) (adjoin-set (cdr set1) (cdr set2))))))
+		(else
+			(sort-asc (cons (car set1) (adjoin-set (cdr set1) (cdr set2)))))))
+
 (element-of-set? 4 (list 1 3 6 10))
 (element-of-set? 3 (list 1 3 6 10))
 (intersection-set (list 1 3 6 10) (list 5 6 7 8 10))
+(adjoin-set (list 1 3 6 10) (list 1 3 6 9 11)) 
