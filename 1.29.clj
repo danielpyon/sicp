0eea24e5eb7be8d38829c5f1d5c184eda5226378
dp <daniel@quicksat.com>
2021-10-14 13:39:40 -0700

    much better

--- 1.29.clj
+++ 1.29.clj
@@ -7,27 +7,14 @@
 
 ; h/3 (f(a) + 4f(a+h) + 2f(a+2h) + 4f(a+3h) + ... f(a + nh))
 
-
-(defn incrementer
-  []
-  (let [counter (atom 0)]
-    (fn [] (do (swap! counter inc) @counter))))
-
 (defn integral
   [f a b n] ; n is the number of slices
-  (let [h (/ (- b a) n)
-        term ((fn []
-                (let [counter (atom -1)]
-                  (fn [x] (do (swap! counter inc)
-                              (* (f x) (cond (= @counter 0) 1
-                                             (= @counter (dec b)) 1
-                                             (= (rem @counter 2) 1) 4
-                                             (= (rem @counter 2) 0) 2)))))))
-        next ((fn []
-               (let [counter (atom -1)]
-                 (fn [x] (do (swap! counter inc)
-                            (+ x (* h @counter)))))))]
-    (* (/ h 3)
-       (sum term a next b))))
+    (let [h (/ (- b a) n)
+          add-2h #(+ % h h)]
+      (* (/ h 3)
+         (+ (f a)
+            (* 2 (sum f a add-2h b))
+            (* 4 (sum f (+ a h) add-2h b))
+            (f b)))))
 
 (println (integral #(* % %) 0 1 100))
