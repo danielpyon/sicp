42ebc375f66a749d36c12b340f4ee5eb078780f3
dp <daniel@quicksat.com>
2021-10-14 17:16:34 -0700

    accumulate recursive done

--- 1.32.clj
+++ 1.32.clj
@@ -6,15 +6,27 @@
   ([combiner null-value term a next b]
    (accumulate combiner null-value term a next b null-value)))
 
+(defn accumulate-recursive
+  [combiner null-value term a next b]
+  (if (> a b)
+    null-value
+    (combiner (term a)
+              (accumulate-recursive combiner
+                                    null-value
+                                    term
+                                    (next a)
+                                    next
+                                    b))))
+
 (defn sum
   [term a next b]
-  (accumulate + 0 term a next b))
+  (accumulate-recursive + 0 term a next b))
 
 (defn product
   [term a next b]
-  (accumulate * 1 term a next b))
+  (accumulate-recursive * 1 term a next b))
 
-(println (accumulate + 0 identity 1 inc 10))
+(println (accumulate-recursive + 0 identity 1 inc 10))
 
 (println (sum identity 1 inc 10))
 (println (product identity 1 inc 10))
