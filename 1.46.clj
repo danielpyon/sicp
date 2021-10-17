a6aa5c7a16d68b5bd8a1356485b55119c0f49e5b
dp <daniel@quicksat.com>
2021-10-16 19:28:00 -0700

    sqrt done

--- 1.46.clj
+++ 1.46.clj
@@ -5,5 +5,14 @@
      guess
      (recur (improve guess)))))
 
+(defn sqrt
+  [x]
+  (defn average [& nums]
+    (/ (reduce + nums) (count nums)))
+  (def tolerance 0.00001)
+  (iterative-improve #(< (Math/abs (- (* % %) x)) tolerance)
+                     #(average % (/ x %))))
 
- 
+
+
+(println ((sqrt 100) 1.0))
