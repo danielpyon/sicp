c8278511f840070d75bd8176f29e36b02d4e168f
dp <daniel@quicksat.com>
2021-10-16 19:43:42 -0700

    completed fixed-point rewrite using iterative-improve

--- 1.46.clj
+++ 1.46.clj
@@ -5,14 +5,32 @@
      guess
      (recur (improve guess)))))
 
+(defn iterative-improve-two-args
+ [good-enough? improve]
+ (fn [guess prev]
+   (if (good-enough? guess prev)
+     guess
+     (recur (improve guess) guess))))
+
+(defn average [& nums]
+  (/ (reduce + nums) (count nums)))
+
 (defn sqrt
   [x]
-  (defn average [& nums]
-    (/ (reduce + nums) (count nums)))
   (def tolerance 0.00001)
-  (iterative-improve #(< (Math/abs (- (* % %) x)) tolerance)
-                     #(average % (/ x %))))
+  ((iterative-improve #(< (Math/abs (- (* % %) x)) tolerance)
+                      #(average % (/ x %)))
+   1.0))
 
+(defn fixed-point
+  [f first-guess]
+  (def tolerance 0.00001)
+  ((iterative-improve-two-args #(< (Math/abs (- %1 %2)) tolerance)
+                               f)
+   first-guess 0))
 
+(println (sqrt 100))
+(println (fixed-point #(average % (/ (Math/log 1000)
+                                     (Math/log %)))
+                      2.0))
 
-(println ((sqrt 100) 1.0))
